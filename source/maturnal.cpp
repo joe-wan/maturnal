04fe46e566a4932b75fcbed5a6d036dcc1d698fa
Joe Wan <joewan94@gmail.com>
2017-03-07 22:59:26 -0800

    Enable arbitrary options

--- source/maturnal.cpp
+++ source/maturnal.cpp
@@ -9,6 +9,7 @@
 
 #include "maturnal.h"
 #include "mothurout.h"
+#include "jwutil.hpp"
 
 #include "classifyseqscommand.h"
 
@@ -18,44 +19,54 @@ MothurOut* MothurOut::_uniqueInstance = 0;
 
 /***********************************************************************/
 int main(int argc, char *argv[]){
-
+	// Tell the program which version/release it is
 	string releaseDate = RELEASE_DATE;
 	string mothurVersion = VERSION;
 	MothurOut* m = MothurOut::getInstance();
 	m->setReleaseDate(releaseDate);
 	m->setVersion(mothurVersion);
 
-  if (argc == 7) {
-    string taxonomyFileName = argv[1];
-    string referenceFileName = argv[2];
-    string search = "kmer";
-    string kmerSize = argv[3];
-    string cutoff = argv[4];
-    string iters = argv[5];
-    bool flip = 0;
-    bool writeShortcuts = 1;
-		string inputFile = argv[6];
-
-    cout << taxonomyFileName << '\t' <<
-				    referenceFileName << '\t' <<
-				    search << '\t' <<
-				    kmerSize << '\t' <<
-				    cutoff << '\t' <<
-				    iters << '\t' <<
-				    inputFile << '\t' <<
-				    endl;
-		map<string, string> parameters = map<std::string, string>();
-		parameters["method"] = "wang";
-		parameters["taxonomy"] = taxonomyFileName;
-		parameters["reference"] = referenceFileName;
-		parameters["ksize"] = kmerSize;
-		parameters["cutoff"] = cutoff;
-		parameters["iters"] = iters;
-		parameters["fasta"] = inputFile;
-		ClassifySeqsCommand* command = new ClassifySeqsCommand(parameters);
-		command->execute();
-		delete command;
-  }
+	// Parse arguments (expect '--') into a map<string, string>
+	try {
+		map<string, string> parameters = jwutil::argvToMap(argc, argv);
+		ClassifySeqsCommand command = ClassifySeqsCommand(parameters);
+		command.execute();
+	} catch (string message) {
+		m->mothurOut(message);
+		m->mothurOutEndLine();
+		return 1;
+	}
+  // if (argc == 7) {
+  //   string taxonomyFileName = argv[1];
+  //   string referenceFileName = argv[2];
+  //   string search = "kmer";
+  //   string kmerSize = argv[3];
+  //   string cutoff = argv[4];
+  //   string iters = argv[5];
+  //   bool flip = 0;
+  //   bool writeShortcuts = 1;
+	// 	string inputFile = argv[6];
+	//
+  //   cout << taxonomyFileName << '\t' <<
+	// 			    referenceFileName << '\t' <<
+	// 			    search << '\t' <<
+	// 			    kmerSize << '\t' <<
+	// 			    cutoff << '\t' <<
+	// 			    iters << '\t' <<
+	// 			    inputFile << '\t' <<
+	// 			    endl;
+	// 	map<string, string> parameters = map<std::string, string>();
+	// 	parameters["method"] = "wang";
+	// 	parameters["taxonomy"] = taxonomyFileName;
+	// 	parameters["reference"] = referenceFileName;
+	// 	parameters["ksize"] = kmerSize;
+	// 	parameters["cutoff"] = cutoff;
+	// 	parameters["iters"] = iters;
+	// 	parameters["fasta"] = inputFile;
+	// 	ClassifySeqsCommand* command = new ClassifySeqsCommand(parameters);
+	// 	command->execute();
+	// 	delete command;
+  // }
 
 	return 0;
 }
